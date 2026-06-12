fc7a3bcc54888a1de5113ada9be84ae3bc7170fd
studiomonira-stack <studiomonira@gmail.com>
2026-06-12 13:47:26 +0200

    Added language switcher + English & Norwegian translations

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import i18n
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('checklist/', include('planner.urls')),
     path('', include('accounts.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
