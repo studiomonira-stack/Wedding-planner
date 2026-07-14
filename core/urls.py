284bf8e4b3ac7dab5112f45c989e14702e609001
studiomonira-stack <studiomonira@gmail.com>
2026-07-14 15:06:41 +0200

    Lade till sitemaps och robots.txt

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from planner import views as planner_views
 from planner.views import partner_page
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {'static': StaticViewSitemap}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,6 +23,8 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')), 
     path('rosetta/', include('rosetta.urls')),
     path('p/<slug:slug>/', partner_page, name='partner_page'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     
     
 ]
@@ -26,3 +33,5 @@ from django.views.decorators.csrf import csrf_exempt
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
